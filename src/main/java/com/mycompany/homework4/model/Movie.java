da0593a2e8ed0f4b352fa59b4e7d174a56de71f9
Amey Satwe <amey10051999@gmail.com>
2024-10-27 14:41:27 -0400

    Start Homework 4

---
+++ src/main/java/com/mycompany/homework4/model/Movie.java
@@ -0,0 +1,50 @@
+package com.mycompany.homework4.model;
+
+public class Movie {
+
+    private String title;
+    private String actor;
+    private String genre;
+    private String director;
+    private Integer releaseYear;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getActor() {
+        return actor;
+    }
+
+    public void setActor(String actor) {
+        this.actor = actor;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public Integer getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(Integer releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+}
